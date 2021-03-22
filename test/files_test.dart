0f602b760fbd9d7a665718d20eba034197c71ee7
rtmigo <art.galkin@gmail.com>
2021-03-22 21:56:09 +0300

    initial

--- test/files_test.dart
+++ test/files_test.dart
@@ -9,6 +9,8 @@ import 'package:test/test.dart';
 
 //import '../lib/10_files.dart';
 
+//import '../lib/10_files.dart';
+
 void main() {
 
   late Directory tempDir;
@@ -21,91 +23,111 @@ void main() {
     tempDir.deleteSync(recursive: true);
   });
 
-  test('list non-existent directory', ()  {
+  void testErrorCode(String testName, bool Function(int x) isXxx, void Function() errorAction, ) {
+    bool caught = false;
+    try {
+      errorAction();
+    } on FileSystemException catch (e)
+    {
+      caught = true;
+      expect(isXxx(e.osError!.errorCode), isTrue);
+    }
+    expect(caught, true);
+  }
+
+  testErrorCode('list non-existent directory', isDirectoryNotExistsCode, () {
     final nonExistentDir = Directory(path.join(tempDir.path, 'nonExistent'));
-    bool caught = false;
-    try {
-      nonExistentDir.listSync();
-    } on FileSystemException catch (e)
-    {
-      caught = true;
-      expect(isDirectoryNotExistsCode(e.osError!.errorCode), isTrue);
-    }
-    expect(caught, true);
+    nonExistentDir.listSync();
   });
 
-  test('open file for reading in non-existent directory', () {
-    // same as open non-existent file
-    bool caught = false;
-    try {
-      File(path.join(tempDir.path, 'non_existent/file.txt')).openSync(mode: FileMode.read);
-    } on FileSystemException catch (e) {
-      caught = true;
-      // ubu
-      //    FileSystemException: Cannot open file, path = '/tmp/IOYRHX/non_existent/file.txt'
-      //    (OS Error: No such file or directory, errno = 2)
-      //
-      //throw;
-      expect(isDirectoryNotExistsCode(e.osError!.errorCode), isTrue);
-
-    }
-    expect(caught, true);
-  });
-
-  test('delete non-empty directory', () {
-    File(path.join(tempDir.path, 'file.txt')).writeAsStringSync('^_^');
-
-    bool caught = false;
-    try {
-      tempDir.deleteSync();
-    } on FileSystemException catch (e)
-    {
-      caught = true;
-      expect(isDirectoryNotEmptyCode(e.osError!.errorCode), isTrue);
-    }
-    expect(caught, true);
-  });
-
-  group('file not exists |', ()
-  {
-    test('open non-existent file', () {
-      bool caught = false;
-      try {
-        File(path.join(tempDir.path, 'file.txt')).openSync(mode: FileMode.read);
-      } on FileSystemException catch (e) {
-        caught = true;
-        expect(isFileNotExistsCode(e.osError!.errorCode), isTrue);
-      }
-      expect(caught, true);
-    });
 
 
 
-    test('open file for writing in non-existent directory', () {
-      // same as open non-existent file
-      bool caught = false;
-      try {
-        File(path.join(tempDir.path, 'non_existent/file.txt')).openSync(mode: FileMode.write);
-      } on FileSystemException catch (e) {
-        caught = true;
-        expect(isFileNotExistsCode(e.osError!.errorCode), isTrue);
-      }
-      expect(caught, true);
-    });
-
-    // test('create file for writing in non-existent directory', () {
-    //   // same as open non-existent file
-    //   bool caught = false;
-    //   try {
-    //     File(path.join(tempDir.path, 'non_existent/file.txt')).createSync();
-    //   } on FileSystemException catch (e) {
-    //     caught = true;
-    //     expect(isFileNotExistsCode(e.osError!.errorCode), isTrue);
-    //   }
-    //   expect(caught, true);
-    // });
-
-  });
+  // test('list non-existent directory', ()  {
+  //   final nonExistentDir = Directory(path.join(tempDir.path, 'nonExistent'));
+  //   bool caught = false;
+  //   try {
+  //     nonExistentDir.listSync();
+  //   } on FileSystemException catch (e)
+  //   {
+  //     caught = true;
+  //     expect(isDirectoryNotExistsCode(e.osError!.errorCode), isTrue);
+  //   }
+  //   expect(caught, true);
+  // });
+  //
+  // test('open file for reading in non-existent directory', () {
+  //   // same as open non-existent file
+  //   bool caught = false;
+  //   try {
+  //     File(path.join(tempDir.path, 'non_existent/file.txt')).openSync(mode: FileMode.read);
+  //   } on FileSystemException catch (e) {
+  //     caught = true;
+  //     // ubu
+  //     //    FileSystemException: Cannot open file, path = '/tmp/IOYRHX/non_existent/file.txt'
+  //     //    (OS Error: No such file or directory, errno = 2)
+  //     //
+  //     //throw;
+  //     expect(isDirectoryNotExistsCode(e.osError!.errorCode), isTrue);
+  //
+  //   }
+  //   expect(caught, true);
+  // });
+  //
+  // test('delete non-empty directory', () {
+  //   File(path.join(tempDir.path, 'file.txt')).writeAsStringSync('^_^');
+  //
+  //   bool caught = false;
+  //   try {
+  //     tempDir.deleteSync();
+  //   } on FileSystemException catch (e)
+  //   {
+  //     caught = true;
+  //     expect(isDirectoryNotEmptyCode(e.osError!.errorCode), isTrue);
+  //   }
+  //   expect(caught, true);
+  // });
+  //
+  // group('file not exists |', ()
+  // {
+  //   test('open non-existent file', () {
+  //     bool caught = false;
+  //     try {
+  //       File(path.join(tempDir.path, 'file.txt')).openSync(mode: FileMode.read);
+  //     } on FileSystemException catch (e) {
+  //       caught = true;
+  //       expect(isFileNotExistsCode(e.osError!.errorCode), isTrue);
+  //     }
+  //     expect(caught, true);
+  //   });
+  //
+  //
+  //
+  //   // test('open file for writing in non-existent directory', () {
+  //   //   // same as open non-existent file
+  //   //   bool caught = false;
+  //   //   try {
+  //   //     File(path.join(tempDir.path, 'non_existent/file.txt')).openSync(mode: FileMode.write);
+  //   //   } on FileSystemException catch (e) {
+  //   //     caught = true;
+  //   //     expect(isFileNotExistsCode(e.osError!.errorCode), isTrue);
+  //   //   }
+  //   //   expect(caught, true);
+  //   // });
+  //
+  //   // test('create file for writing in non-existent directory', () {
+  //   //   // same as open non-existent file
+  //   //   bool caught = false;
+  //   //   try {
+  //   //     File(path.join(tempDir.path, 'non_existent/file.txt')).createSync();
+  //   //   } on FileSystemException catch (e) {
+  //   //     caught = true;
+  //   //     expect(isFileNotExistsCode(e.osError!.errorCode), isTrue);
+  //   //   }
+  //   //   expect(caught, true);
+  //   // });
+  //
+  // });
 
 
 }
