d22941d8b390fc4feb7ee87df803dbe1cfe24761
rtmigo <art.galkin@gmail.com>
2021-03-24 11:44:21 +0300

    initial

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -12,11 +12,16 @@ bool _checkCode(FileSystemException exception, bool Function(int) check) {
   return (code != null) && check(code);
 }
 
+/// Adds cross-platform extensions that give explanations to common problems when accessing files.
 extension FileSystemExceptionExplanation on FileSystemException {
+
+  /// Occurs when the requested file or directory does not exist;
+  /// or its parent directory does not exist.
   bool get isNoSuchFileOrDirectory {
     return _checkCode(this, isNoSuchFileOrDirectoryCode);
   }
 
+  /// Occurs when you try to non-recursively delete a directory but it contains files.
   bool get isDirectoryNotEmpty {
     return _checkCode(this, isNotEmptyCode);
   }
